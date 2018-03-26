f1dbe7ac699308036aa3a539eb0c1051e29762b5
debris <124509217@qq.com>
2018-03-26 09:39:05 +0800

    update

---
+++ src/components/login/login.vue
@@ -0,0 +1,20 @@
+<template>
+    <!--登陆组件-->
+    <div id="login">
+        <div class="login_icon"></div>
+        <p class="login_detail">欢迎来到“爱习惯”习惯培养学校</p>
+    </div>
+</template>
+<script>
+export default {
+    mixins: [window.mixin],
+    data() {
+        return {
+            "pageName": "登陆"
+        }
+    }
+}
+</script>
+    <style>
+        @import "../../assets/css/login.css";
+    </style>
